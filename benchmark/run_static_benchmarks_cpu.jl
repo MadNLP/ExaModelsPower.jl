ad1514a29f7ce4883f2a060e4fc482f2852762f8
SanjayJohnson2 <sanjay.johnson@hotmail.com>
2025-08-28 16:00:12 -0400

    adding data

--- benchmark/run_static_benchmarks_cpu.jl
+++ benchmark/run_static_benchmarks_cpu.jl
@@ -1,9 +1,7 @@
 using ExaModelsPower, MadNLP, MadNLPGPU, ExaModels
 include("benchmark_opf.jl")
 for coord in ["Polar", "Rectangular"]
-    for tol in [1e-4, 1e-6, 1e-8]
-        for style in ["default", "api", "sad"]
-            solve_static_cases(cases, tol, coord, "CPU"; case_style = style)
-        end
+    for tol in [1e-4, 1e-8]
+        solve_static_cases(cases, tol, coord, "CPU";)
     end
 end
--- benchmark/run_static_benchmarks_gpu.jl
+++ benchmark/run_static_benchmarks_gpu.jl
@@ -1,10 +1,9 @@
 using ExaModelsPower, MadNLP, MadNLPGPU, CUDA, ExaModels
 println("Using device: ", CUDA.device())
 include("benchmark_opf.jl")
-for coord in ["Rectangular"]#["Polar", "Rectangular"]
-    for tol in [1e-4, 1e-6, 1e-8]
-        for style in ["default", "api", "sad"]
-            solve_static_cases(cases, tol, coord, "GPU"; case_style = style)
+for coord in ["Polar", "Rectangular"]
+    for tol in [1e-4, 1e-8]
+            solve_benchmark_cases(cases, tol, "GPU"; coords = coord)
         end
     end
 end
