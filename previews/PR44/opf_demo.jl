cdd6bdfe2dede316e4c371284a9661e443b9cb4f
Documenter.jl <documenter@juliadocs.github.io>
2026-06-01 11:30:53 -0500

    build based on 7352ad9

--- previews/PR44/opf_demo.jl
+++ previews/PR44/opf_demo.jl
@@ -5,8 +5,8 @@
 # The latest version of ExaModelsPower can be installed in julia as so. Additionally, in order to develop models that can be solved on the GPU, CUDA is required. 
 using ExaModelsPower, CUDA
 
-# In order to solve the ExaModels developed by ExaModelsPower, an NLP solver is required. ExaModels is compatible with MadNLP and Ipopt, but this tutorial will focus on MadNLP to demonstrate GPU solving capabilities.
-using MadNLP, MadNLPGPU #, NLPModelsIpopt 
+# In order to solve the ExaModels developed by ExaModelsPower, an NLP solver is required. ExaModels is compatible with MadNLP and Ipopt, but this tutorial will focus on MadNLP to demonstrate GPU solving capabilities. Loading CUDSS activates the cuDSS sparse linear solver used for the GPU solves below.
+using MadNLP, MadNLPGPU, CUDSS #, NLPModelsIpopt
 
 # Finally, we install ExaModels to allow solved models to be unpacked.
 using ExaModels
@@ -20,8 +20,8 @@ model, vars, cons = ac_opf_model(
 );
 model
 
-# Once the model is built, we can generate a solution using MadNLP.
-result = madnlp(model; tol=1e-6)
+# Once the model is built, we can generate a solution using MadNLP. Because this model lives on the GPU, we select the condensed KKT system and the cuDSS linear solver so the factorization runs on the device (the MadNLP defaults are CPU-only).
+result = madnlp(model; tol=1e-6, kkt_system = MadNLP.SparseCondensedKKTSystem, linear_solver = MadNLPGPU.CUDSSSolver)
 
 # Once a solution has been generated, the values of any of the variables in the model can be unpacked using the vars NamedTuple.
 solution(result, vars.vm)[1:10]
